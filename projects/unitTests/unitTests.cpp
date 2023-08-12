8625a74150efbe9babaa22833f24077a6ee3a267
Alvf <alvin.shi@gmail.com>
2023-08-12 12:46:59 -0400

    Trimmed unitTests

---
+++ projects/unitTests/unitTests.cpp
@@ -0,0 +1,19 @@
+#include <cmath>
+#include <iostream>
+#include <float.h>
+
+#define CATCH_CONFIG_MAIN
+
+#include "util/MATRIX_UTIL.h"
+#include "Collision/C_PLANES_EE.h"
+#include "Collision/C_SPECIAL_EE.h"
+#include "Collision/ENERGY_12D.h"
+#include "Collision/COLLISION_UTIL.h"
+#include "Collision/UNSIGNED_LEN_PLANES.h"
+#include "Collision/UNSIGNED_SPRING_1D.h"
+#include "catch_amalgamated.hpp"
+
+using namespace HOBAK;
+using namespace std;
+
+#include "COLLISION_ENERGY_TESTS.h"
